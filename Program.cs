527b731291b7d8b35cb944a47a4625e67992c5d1
The-Man-M <143039832+The-Man-M@users.noreply.github.com>
2023-10-09 19:35:59 -0600

    version 0.1

--- Program.cs
+++ Program.cs
@@ -4,9 +4,48 @@
     {
         static void Main(string[] args)
         {
+            int num_jugador = 0, num_enemigo = 0;
+            num_enemigo = num_aleatorio();
+            do
+            {
+                inicio();
+
+                if(num_jugador > num_enemigo)
+                {
+
+                }
+                else if (num_jugador < num_enemigo)
+                {
+
+                }
+                else
+                {
+
+                }
+
+            } while (validar(num_jugador,num_enemigo)) ;
+
+            
+        }
+        static int num_aleatorio()
+        {
+            Random num = new Random();
+            return num.Next(1,10);
+        }
+        static bool validar(int num_jugador, int num_enemigo)
+        {
+            if (num_jugador == num_enemigo) return true;
+            else return false;
+        }
+        static void inicio()
+        {
             Console.BackgroundColor = ConsoleColor.Red;
             Console.WriteLine("ADIVINA EL NUMERO");
-            
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine("Rules");
+            Console.WriteLine("Tienes 6 intentos\nEl numero estará en un rango de 1 a 10.");
+
+            Console.WriteLine("EMPEZEMOS");
         }
     }
 }
